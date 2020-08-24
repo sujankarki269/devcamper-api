da63e1e1a56ecd50ebf101d08948726c3c55e54e
sujan karki <sujankarki269@gmail.com>
2020-08-24 20:15:46 +0545

    commit

--- server.js
+++ server.js
@@ -1,20 +1,44 @@
 
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+const path = require('path');
 
-
-
+const connectDb = require('./config/db');
+//Route files
+const bootcamps = require('./routes/bootcamps');
 //Load env vars
-dotenv.config({
-    path: './config/config.env'
-});
+dotenv.config({ path: './config/.env'});
 
+
+//connect to DB
+connectDb();
 
 const app = express();
 
+//Body Parser
+app.use(express.json());
+
+//Dev logging middleware
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
+
+//Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server= app.listen(
     PORT,
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
 );
+
+//Handle unhandled promise rejection
+process.on('unhandledRejection',(err, promise)=>{
+    console.log(`Error: ${err.message}`.red);
+    //Close server & exit process
+    server.close(()=>process.exit(1));
+});
