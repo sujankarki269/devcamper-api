22d074f520db5e083d39b7e3a7ebea1a6086a922
sujan karki <sujankarki269@gmail.com>
2020-08-29 14:05:31 +0545

    commit

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -4,19 +4,39 @@ const {
     getBootcamp,
     createBootcamp,
     updateBootcamp,
-    deleteBootcamp
+    deleteBootcamp,
+    getBootcampsInRadius,
+    bootcampPhotoUpload
 } = require('../controllers/bootcampController');
+
+const Bootcamp = require('../models/Bootcamp');
+
+const advancedResults = require('../middleware/advancedResults');
+
+// Include other resource routers
+const courseRouter = require('./courses');
+
+
 const router = express.Router();
 
+//Re-route into other resource routers
+router.use('/:bootcampId/courses', courseRouter);
 
-router .route('/')
-.get(getBootcamps)
-.post(createBootcamp);
+router.route('/radius/:zipcode/:distance')
+    .get(getBootcampsInRadius);
+
+router.route('/:id/photo')
+    .put(bootcampPhotoUpload);
+
+
+router.route('/')
+    .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
+    .post(createBootcamp);
 
 router.route('/:id')
-.get(getBootcamp)
-.put(updateBootcamp)
-.delete(deleteBootcamp);
+    .get(getBootcamp)
+    .put(updateBootcamp)
+    .delete(deleteBootcamp);
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const colors = require('colors');
+const fileupload = require('express-fileupload');
 const errorHandler = require('./middleware/error');
 
 const connectDb = require('./config/db');
 //Route files
 const bootcamps = require('./routes/bootcamps');
+const courses = require('./routes/courses');
 //Load env vars
 dotenv.config({ path: './config/.env'});
 
@@ -24,9 +27,17 @@ app.use(express.json());
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'));
 }
+// File uploading
+app.use(fileupload());
+
+
+//Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
+app.use('/api/v1/courses', courses);
+
 
 app.use(errorHandler);
 
