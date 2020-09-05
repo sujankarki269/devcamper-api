4d83a8e38fea6b093fc207c6af9f466718d9cd72
sujan karki <sujankarki269@gmail.com>
2020-09-05 14:22:37 +0545

    commit

--- routes/auth.js
+++ routes/auth.js
@@ -4,10 +4,10 @@ const {
     login,
     // logout,
     getMe,
-    // forgotPassword,
-    // resetPassword,
-    // updateDetails,
-    // updatePassword,
+    forgotPassword,
+    resetPassword,
+    updateDetails,
+    updatePassword,
     // confirmEmail,
 } = require('../controllers/authController');
 
@@ -20,5 +20,9 @@ router.post('/register', register);
 router.post('/login', login);
 
 router.get('/me', protect, getMe);
+router.put('/updatedetails', protect, updateDetails);
+router.put('/updatepassword', protect, updatePassword);
+router.post('/forgotpassword', forgotPassword);
+router.put('/resetpassword/:resettoken', resetPassword);
 
 module.exports = router;
