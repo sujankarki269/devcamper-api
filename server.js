c538d1ecc5e1d53d0f2017374172d9c75f398e2d
sujan karki <sujankarki269@gmail.com>
2020-09-06 09:55:10 +0545

    commit

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const connectDb = require('./config/db');
 const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
 const auth = require('./routes/auth');
+const users = require('./routes/users');
 
 
 //Load env vars
@@ -47,6 +48,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
 app.use('/api/v1/auth', auth);
+app.use('/api/v1/users', users);
 
 
 app.use(errorHandler);
